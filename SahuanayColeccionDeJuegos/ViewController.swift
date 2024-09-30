b9aa3d62e62cafa283a6861ed69c71acd6bdd597
Sahuanay <alonso.sahuanay@tecsup.edu.pe>
2024-09-30 17:21:50 -0500

    Initial Commit

---
+++ SahuanayColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SahuanayColeccionDeJuegos
+//
+//  Created by Alonso Mijail Sahuanay Huacan on 30/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
